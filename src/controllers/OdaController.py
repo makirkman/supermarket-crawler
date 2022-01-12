d9ed100e3b7449b8e4c06989c726c3c1d5a3c8f7
Max <32726290+Maekseu@users.noreply.github.com>
2022-01-12 12:15:52 +0100

    Refactored OdaController to remove some unnecessary checks, after testing with and without for no difference.

--- src/controllers/OdaController.py
+++ src/controllers/OdaController.py
@@ -20,30 +20,21 @@ class OdaController(SiteController):
 		On the oda site, you know you are on a dedicated product page when the
 		 full categories list is no longer showing (just one category), and
 		 there are also no in-page child category links on the page.
-
-		At the moment there are about 20 items which are not found if this guard
-		 is used before reading products on a page. More investigation needed
-		 as to why. Using the guard saves about 20 seconds from run time.
 		"""
+
+		# At the moment there are about 20 items which are not found if this guard
+		#  is used before reading products on a page. More investigation needed
+		#  as to why. Using the guard saves about 20 seconds from run time.
+		return True
+
 		# check if the full category list on the left is hidden
 		# if this class exists, the category list is hidden, otherwise it's visible
-		# return True
 		category_list = soup.find('li', {'class': 'product-category parent-category visible-xs-block'})
 		print('category list')
 		print(category_list)
 		if not category_list:
 			return False
 
-		# now if there is a top nav bar, one of the pills must be active
-		# top_nav_bar = soup.find('ul', {'class': 'nav nav-pills'})
-
-		# print('top nav bar')
-		# print(top_nav_bar.find_all('a'))
-		# if not top_nav_bar.find_all('a'):
-		# 	return True
-
-		# if top_nav_bar.find('li', {'class': 'active'}):
-		# 	return True
 		# if there are child category headings, it also isn't a product page
 		child_categories = soup.find_all('h4', {'class': 'child-category-headline'})
 		if child_categories:
@@ -59,19 +50,13 @@ class OdaController(SiteController):
 		Returns the list of unignored links.
 		"""
 		next_links: list[str] = []
+
 		# if there is a next product page button, then always add it
 		next_page = soup.find('a', {'title': 'Neste side'})
 		if next_page:
 			# needs to use the current url because the link is just a param
 			next_links.append(cur_url + next_page.get('href'))
 
-		# # if there is a top nav bar, and one of them hasn't been clicked, click it
-		# top_nav_bar_a = soup.find('ul', {'class': 'nav nav-pills'}).find_all('a')
-
-		# if top_nav_bar_a:
-		# 	nav_pill_links = [pill.get('href') for pill in top_nav_bar_a]
-		# 	# find an unvisited nav pill if one exists
-		# 	next_links += self._find_unvisited_urls(nav_pill_links, urls_to_ignore)
 
 		# if there are any in-page child category links, add them
 		child_categories = soup.find_all('h4', {'class': 'child-category-headline'})
@@ -82,11 +67,10 @@ class OdaController(SiteController):
 			if child_a:
 				child_as.append(child_a)
 
-		if child_categories:
-			print(child_categories)
-			child_links = [child.get('href') for child in child_as]
-			# find an unvisited in-page category if one exists
-			next_links += self._find_unvisited_urls(child_links, urls_to_ignore)
+		child_links = [child.get('href') for child in child_as]
+		# find an unvisited in-page category if one exists
+		next_links += self._find_unvisited_urls(child_links, urls_to_ignore)
+
 
 		# if there are any sub-categories on the category nav bar, add them
 		sub_categories = soup.find_all('li', {'class': 'child-category'})
@@ -95,34 +79,26 @@ class OdaController(SiteController):
 			# find an unvisited sub-category if one exists
 			next_links +=  self._find_unvisited_urls(sub_links, urls_to_ignore)
 
+
 		# if there are no more sub-categories, add the next categories
-		# TODO: Is this necessary? These have aggfilterheadline too
-		categories = soup.find_all('li', {'class': 'product-category parent-category'})
-		if categories:
-			cat_links = [cat.find('a').get('href') for cat in categories]
-			# find an unvisited category if one exists
-			next_links += self._find_unvisited_urls(cat_links, urls_to_ignore)
-
-		# also add the special parent categories
-		par_categories = soup.find_all('h4', {'class': 'aggregation-filter-headline'})
+		categories = soup.find_all('h4', {'class': 'aggregation-filter-headline'})
 		# find all the links under the headlines (ignore headlines without links)
-		par_as = []
-		for par in par_categories:
-			par_a = par.find('a')
-			if par_a:
-				par_as.append(par_a)
+		cat_as = []
+		for par in categories:
+			cat_a = par.find('a')
+			if cat_a:
+				cat_as.append(cat_a)
 
-		# TODO: Do we need a guard here?
-		if par_categories:
-			print(par_as)
-			par_links = [par.get('href') for par in par_as]
-			# find an unvisited parent category if one exists
-			next_links += self._find_unvisited_urls(par_links, urls_to_ignore)
+		cat_links = [par.get('href') for par in cat_as]
+		# find an unvisited category if one exists
+		next_links += self._find_unvisited_urls(cat_links, urls_to_ignore)
 
 		return next_links
 
 	def get_all_product_items_in_page(self, soup):
-		items = soup.findAll('div', {'class': 'product-list-item'})
+		# items = soup.findAll('div', {'class': 'product-list-item'})
+		# special_items = soup.findAll('div', {'class': 'product-list-item'})
+		items = soup.select('div[class*=product-list-item]')
 
 		oda_items = []
 		for item in items:
@@ -141,7 +117,7 @@ class OdaController(SiteController):
 
 		# check that each piece of metadata was found before continuing
 		#  a missing name is an error, but the rest can be None
-		assert name.string, "Found a product which does not have a name - this is not supported behaviour. Please investigate."
+		assert name.string, f"Found a product which does not have a name - this is not supported behaviour. Please investigate. Product html was:\n{soup.prettify()}"
 		name = name.string.strip()
 		if price and price.string:
 			price = price.string.strip()
